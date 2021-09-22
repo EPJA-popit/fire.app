949da41fc5cffeefb5acfea3eb9ab435ed3644a7
Egor <egor.osokin7@gmail.com>
2021-09-23 00:07:14 +0300

    chore: add some debugging logging

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,21 @@ import "systemjs/dist/extras/named-register";
 import "systemjs/dist/extras/named-exports";
 import "systemjs/dist/extras/transform";
 
-export default async ({ apps, navigations, config }) => {
+export default async ({ apps, navigation, config }) => {
   const appName = location.pathname.replace("/", "");
+  if (!(appName in apps)) {
+    console.log(
+      `WARNING: app ${appName} is not provided in apps: ${JSON.stringify(apps)}`
+    );
+  }
   const appPath = `/static/${appName}/${apps[appName].version}/index.js`;
 
-  console.log(appPath);
+  console.log("in fire.app, appPath=", appPath);
 
-  const { default: componenet, mount, unmount } = await System.import(appPath);
+  const { default: component, mount, unmount } = await System.import(appPath);
+  console.log("in fire.app, component, mount=", component, mount);
 
-  mount(componenet.default);
+  mount(component.default);
 };
 
-System.import("/js/main.js");
+// System.import("/src/main.ts");
